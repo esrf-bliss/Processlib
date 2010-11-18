08fc961cdb2a99f51a569a22b193a2c1ae52275d
seb <sebastien.petitdemange@esrf.fr>
2010-11-18 13:39:01 +0100

    add unistd for window

---
+++ core/include/WindowSpecific/unistd.h
@@ -0,0 +1,23 @@
+#ifndef _UNISTD_H
+#define _UNISTD_H        1
+
+/* This file intended to serve as a drop-in replacement for 
+ *  unistd.h on Windows
+ *  Please add functionality as neeeded 
+ */
+
+#include <stdlib.h>
+#include <io.h>
+
+#define srandom srand
+#define random rand
+
+#define W_OK 2
+#define R_OK 4
+
+#define access _access
+#define ftruncate _chsize
+
+#define ssize_t int
+#define usleep Sleep
+#endif /* unistd.h  */
