34f1ceea3a2ffa2db65ce646833abf90cfe916cb
Valentin Valls <valentin.valls@esrf.fr>
2025-03-14 10:15:57 +0100

    Rework as pytest

--- test/test_roicollection.py
+++ test/test_roicollection.py
@@ -2,36 +2,41 @@ from Lima import Core
 import numpy
 import time
 
-a = numpy.ones((2048,2048),dtype=numpy.uint16)
 
-d = Core.Processlib.Data()
-d.frameNumber=0
-d.buffer=a
+def test_roicollection():
+    a = numpy.ones((2048,2048),dtype=numpy.uint16)
 
-m = Core.Processlib.Tasks.RoiCollectionManager()
-xstart=numpy.arange(0,2048,4)
-ystart=numpy.arange(0,2048,4)
+    d = Core.Processlib.Data()
+    d.frameNumber=0
+    d.buffer=a
 
-rois = []
-#create 262144 rois
-for y in ystart:
-    for x in xstart:
-        rois.append((x,y,3,3))
-m.setRois(rois)
+    m = Core.Processlib.Tasks.RoiCollectionManager()
+    xstart=numpy.arange(0,2048,4)
+    ystart=numpy.arange(0,2048,4)
 
-print("Will process 262144 rois on an image 2048x2048-16bpp")
-t0=time.time()
-m.prepare()
-print(f"prepare() took {time.time()-t0:.6f} sec.")
-t0=time.time()
-m.process(d)
-print(f"process() took {time.time()-t0:.6f} sec.")
+    rois = []
+    #create 262144 rois
+    for y in ystart:
+        for x in xstart:
+            rois.append((x,y,3,3))
+    m.setRois(rois)
 
-r=m.getResult(0)
-s=r.spectrum
-print (f"Processed {len(s)} roi counters successfully")
+    print("Will process 262144 rois on an image 2048x2048-16bpp")
+    t0=time.time()
+    m.prepare()
+    print(f"prepare() took {time.time()-t0:.6f} sec.")
+    t0=time.time()
+    m.process(d)
+    print(f"process() took {time.time()-t0:.6f} sec.")
+
+    r=m.getResult(0)
+    s=r.spectrum
+    print (f"Processed {len(s)} roi counters successfully")
 
 
-print (f"check now your RAM usage, we loop on reading the spectrum (issue #22 in Processlib")
-while True:
-  r.spectrum
+def test_ram_usage():
+    print (f"check now your RAM usage, we loop on reading the spectrum (issue #22 in Processlib")
+    m = Core.Processlib.Tasks.RoiCollectionManager()
+    r=m.getResult(0)
+    for _ in range(1_000_000):
+        r.spectrum
